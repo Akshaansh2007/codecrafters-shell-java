77157962dc2a2841be86099911784ad56ee2a95a
Akshaansh Rai <Akshaanshrai2007@gmail.com>
2026-06-15 16:08:56 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,7 @@ public class Main {
             if(a.equals("exit")){break;}
             if(a.startsWith("echo")){
                 System.out.println(a.substring(5));
+                continue;
             }
             System.out.println(a +  ": command not found");
         }
