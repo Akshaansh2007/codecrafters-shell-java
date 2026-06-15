f50eab68d55799a10a4b07f990cec0fe0e150518
Akshaansh Rai <Akshaanshrai2007@gmail.com>
2026-06-15 17:33:31 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
+import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
@@ -10,34 +10,85 @@ public class Main {
         while(true){
             System.out.print("$ ");
             String a = sc.nextLine();
-            if(a.equals("exit")){break;}
-            if(a.startsWith("echo")){
-                System.out.println(a.substring(5));
-                continue;
+            if (a.isEmpty()) continue;
+            String[] parts = a.trim().split("\\s+");  
+            String command = parts[0];
+            if(command.equals("echo") || command.equals("exit")){
+                if(command.equals("echo")){
+                    for (int i = 1; i < parts.length; i++) {
+                    System.out.print(parts[i]);
+                    if (i < parts.length - 1) System.out.print(" ");
+                    }
+                    System.out.println();
+                }   
+                else if(command.equals("exit")){
+                    break;
+                }
             }
-            if(a.startsWith("type")){
-                String command = a.substring(5);
-                if(command.equals("exit") || command.equals("echo") || command.equals("type")){
-                    System.out.println(command + " is a shell builtin");
+            else if(command.equals("type")){ 
+                
+                if(parts.length < 2){
+                    System.out.println("type: missing argument");
                     continue;
                 }
+
+                String target = parts[1];
+                
+                if(target.equals("echo") || target.equals("exit") || target.equals("type")){
+                    System.out.println(target + " is a shell builtin");
+                    continue;
+                }
+
+
                 boolean found = false;
 
-                for(String dir : dirs){
-                    Path p = Paths.get(dir, command);
-                    if(Files.exists(p) && Files.isExecutable(p)){
-                        System.out.println(command + " is "+ p);
-                        found = true;
+                for(String dir : dirs){ 
+                    Path p = Paths.get(dir, target); 
+                    if(Files.exists(p) && Files.isExecutable(p)){ 
+                        System.out.println(target + " is "+ p); 
+                        found = true; break; 
+                    } 
+                } 
+                
+                if(found == false){ 
+                    System.out.println(target + ": not found"); 
+                }
+            }
+            else {
+                String fullpath = null;
+
+                for(String dir : dirs){ 
+                    Path p = Paths.get(dir, command); 
+                    if(Files.exists(p) && Files.isExecutable(p)){ 
+                        fullpath = p.toString();
                         break;
-                    }
+                    } 
+                } 
+
+                if(fullpath == null){
+                    System.out.println(command + ": command not found");
+                    continue;
                 }
 
-                if(found == false){
-                    System.out.println(command +  ": not found");
+
+
+                try {
+                    List<String> cmd = new ArrayList<>();
+                    cmd.add(fullpath);
+
+                    for(int i = 1; i < parts.length; i++){
+                        cmd.add(parts[i]);
+                    }
+
+                    ProcessBuilder pb = new ProcessBuilder(cmd);
+                    pb.inheritIO();
+                    Process process = pb.start();
+                    process.waitFor();
+                } 
+                catch (Exception e) {
+                    System.out.println("Error executing command");
                 }
-                continue;
             }
-            System.out.println(a +  ": command not found");
         }
     }
 }
